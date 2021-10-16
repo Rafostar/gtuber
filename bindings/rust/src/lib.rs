6337ed856f5421e64e27815891c3c2b57f32c3f8
Florian "sp1rit"​ <sp1ritCS@protonmail.com>
2021-10-16 15:09:24 +0200

    bindings: added rust bindings

---
+++ bindings/rust/src/lib.rs
@@ -0,0 +1,11 @@
+pub use ffi;
+
+#[allow(clippy::clone_on_copy)]
+#[allow(clippy::let_and_return)]
+#[allow(clippy::type_complexity)]
+#[allow(unused_doc_comments)]
+#[allow(unused_imports)]
+mod auto;
+pub use auto::*;
+
+pub mod prelude;
