b25b5d5d77008f9ba146c4fb5a35ffae44c7aa89
Rafał Dzięgiel <rafostar.github@gmail.com>
2021-10-19 22:24:37 +0200

    tests: Add "bilibili" tests

---
+++ tests/plugins/bilibili.c
@@ -0,0 +1,56 @@
+#include "../tests.h"
+
+GTUBER_TEST_MAIN_START ()
+
+/* BV */
+GTUBER_TEST_CASE (1)
+{
+  GtuberMediaInfo *info = g_object_new (GTUBER_TYPE_MEDIA_INFO, NULL);
+  GtuberMediaInfo *fetch = NULL;
+
+  gtuber_media_info_set_id (info, "344682149");
+  gtuber_media_info_set_title (info, "4621 comercial video!");
+  gtuber_media_info_set_duration (info, 107);
+  compare_fetch (client, "https://www.bilibili.com/video/BV1Ub4y1o7uJ", info, &fetch);
+
+  check_adaptive_streams (fetch);
+
+  g_object_unref (info);
+  g_object_unref (fetch);
+}
+
+/* AV */
+GTUBER_TEST_CASE (2)
+{
+  GtuberMediaInfo *info = g_object_new (GTUBER_TYPE_MEDIA_INFO, NULL);
+  GtuberMediaInfo *fetch = NULL;
+
+  gtuber_media_info_set_id (info, "420863715");
+  gtuber_media_info_set_title (info, "Matrix IM+ typing test");
+  gtuber_media_info_set_duration (info, 193);
+  compare_fetch (client, "https://www.bilibili.com/video/av890934190", info, &fetch);
+
+  check_adaptive_streams (fetch);
+
+  g_object_unref (info);
+  g_object_unref (fetch);
+}
+
+/* Bangumi EP */
+GTUBER_TEST_CASE (3)
+{
+  GtuberMediaInfo *info = g_object_new (GTUBER_TYPE_MEDIA_INFO, NULL);
+  GtuberMediaInfo *fetch = NULL;
+
+  gtuber_media_info_set_id (info, "410355895");
+  gtuber_media_info_set_title (info, "定档PV 10月30日起不见不散");
+  gtuber_media_info_set_duration (info, 82);
+  compare_fetch (client, "https://www.bilibili.com/bangumi/play/ep423320", info, &fetch);
+
+  check_adaptive_streams (fetch);
+
+  g_object_unref (info);
+  g_object_unref (fetch);
+}
+
+GTUBER_TEST_MAIN_END ()
