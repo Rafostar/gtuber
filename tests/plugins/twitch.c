045041ef36f72eacdec4bbd50b32f509f61539ac
Rafał Dzięgiel <rafostar.github@gmail.com>
2022-09-28 17:27:26 +0200

    tests: twitch: Add "clips.twitch.tv" subdomain test

--- tests/plugins/twitch.c
+++ tests/plugins/twitch.c
@@ -43,4 +43,24 @@ GTUBER_TEST_CASE (2)
   g_object_unref (client);
 }
 
+/* Clip from subdomain */
+GTUBER_TEST_CASE (3)
+{
+  GtuberClient *client = gtuber_client_new ();
+  GtuberMediaInfo *info, *out_info = NULL;
+
+  info = g_object_new (GTUBER_TYPE_MEDIA_INFO, NULL);
+
+  gtuber_media_info_set_id (info, "943620356");
+  gtuber_media_info_set_title (info, "Crashing into asteroid POV");
+  compare_fetch (client, "https://clips.twitch.tv/FitSnappyCaribouPhilosoraptor-F7IcTqcQWY2V9SrX", info, &out_info);
+
+  assert_no_adaptive_streams (out_info);
+  check_streams (out_info);
+
+  g_object_unref (info);
+  g_object_unref (out_info);
+  g_object_unref (client);
+}
+
 GTUBER_TEST_MAIN_END ()
